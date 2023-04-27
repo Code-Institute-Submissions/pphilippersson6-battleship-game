0356ab6c7b4e5e764c54f475e0a7de99e4596648
pphilippersson6 <philip.persson.code@gmail.com>
2023-04-27 09:19:01 +0000

    added print msg, and columns

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
