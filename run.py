76d0a77439c6f1855753cae73000aa402b40dae6
pphilippersson6 <philip.persson.code@gmail.com>
2023-05-01 16:33:09 +0000

    re-did my code from start, was making some errors i did not manage to find. Did the bacsic coding and added the platers turn

--- run.py
+++ run.py
@@ -67,6 +67,20 @@ def play_game():
 try:
     guess_row = int(input("Guess Row (0-4): "))
     guess_col = int(input("Guess Col (0-4): "))
-    except ValueError:
+except ValueError:
          print("Please enter a valid integer.")
          continue
+
+if guess_row == ship_row and guess_col == ship_col:
+    print("Congratulations! You sunk my battleship!")
+    break
+else:
+    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        print("Oops, that's not even in the ocean.")
+        elif player_board[guess_row][guess_col] == "X":
+        print("You guessed that one already.")
+    else:
+        print("You missed my battleship!")
+        player_board[guess_row][guess_col] = "X"
+
+        
